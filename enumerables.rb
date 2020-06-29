59f9c4081b21941d8257ac7e17440de3c5611100
umairahmad125 <umairahmad125@gmail.com>
2020-06-29 19:10:02 +0500

    implement my _each ,ethod

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,10 @@
-puts "Hello world"
+# frozen_string_literal: true
+
+module Enumerable
+  def my_each
+    (0...length).each do |i|
+      yield(self[i])
+    end
+  end
+  [1, 2, 3, 6, 5].my_each { |i| puts i }
+end

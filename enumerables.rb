cc11e3cf40213ea8464b2a067830da66e68885d5
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-06-29 15:32:49 +0100

    implement my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -2,9 +2,16 @@
 
 module Enumerable
   def my_each
-    (0...length).each do |i|
+    for i in 0...self.length
       yield(self[i])
     end
   end
   [1, 2, 3, 6, 5].my_each { |i| puts i }
+
+def my_each_with_index 
+   for i in 0...self.length
+    yield(i,self[i])
+      end
+    end
+    [7, 9, 33, 61, 15].my_each_with_index { |i, element| puts "#{i} " "#{element}" }
 end

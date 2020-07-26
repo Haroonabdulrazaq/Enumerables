c4ad31637e6f3b07ce8aefcbabb9b8208a50e7bc
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-07-26 17:44:45 +0100

    Add test for my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,11 +2,14 @@ require './enumerables.rb'
 
 describe Enumerable do 
     let (:arr) {[1,2,5]}   
+    let (:my_arr) {[]}
+    let (:original_arr ) {[]}
     let (:hash) {{"javaScript": 2018, "PHP": 2002, "java": 1995}}
+    let (:my_hash) {[]}
+    let (:original_hash) {[]}
+
     describe "#my_each" do
-        context "If block is given" do
-            my_arr =[]
-            original_arr = []
+        context "If block is given" do        
             it "Iterate over array and returns the iterated array" do
                 arr.my_each{|x| my_arr << x}
                 arr.each{|y| original_arr << y}
@@ -14,8 +17,6 @@ describe Enumerable do
             end
 
             it "Iterate over hash and returns the iterated hash" do
-                my_hash =[]
-                original_hash = []
                 hash = hash.to_a
                 hash.my_each{|key| my_hash << key} 
                 hash.each{|key| original_hash << key}
@@ -26,6 +27,31 @@ describe Enumerable do
         context "If block not given" do
             it "returns enum if block is not given" do
                 expect(arr.my_each.is_a?(Enumerable)).not_to be false
+            end
+        end
+    end
+
+
+
+    describe "#my_each_with_index" do
+        context "If block is given" do
+            it "Iterate over array and returns the iterated array" do
+                arr.my_each_with_index{|x, i| my_arr << x + i}
+                arr.each_with_index{|y, i| original_arr << y + i}
+                expect(my_arr).to eql(original_arr)
+            end
+
+            it "Iterate over hash and returns the iterated hash" do
+                hash = hash.to_a
+                hash.my_each_with_index{|element, index| my_hash << key} 
+                hash.each_with_index{|element, index| original_hash << key}
+                expect(my_hash).to eql(original_hash)
+            end
+        end
+
+        context "If block not given" do
+            it "returns enum if block is not given" do
+                expect(arr.my_each_with_index.is_a?(Enumerable)).not_to be false
             end
         end
     end
